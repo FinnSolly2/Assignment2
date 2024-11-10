f3dd50b05654100928e34e77e426a1bd0528f874
FinnSolly2 <finnsolly2@gmail.com>
2024-11-10 18:22:52 +0100

    Added pricing method for products

--- stock_management.py
+++ stock_management.py
@@ -19,3 +19,7 @@ class StockManager:
     def get_stock_level(self, product_id):
         product = self.inventory.get_product(product_id)
         return product.quantity if product else 0
+
+    def get_current_stock_value(self, product_id, product_price):
+	product = self.inventory.get_product(product_id)
+        return product.quantity * product_price if product else 0		
